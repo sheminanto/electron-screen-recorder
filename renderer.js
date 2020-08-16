1b207d30a587a514f7cea0ddbe5431080f32b696
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2020-08-16 20:53:41 +0530

    enumerate devivices

--- renderer.js
+++ renderer.js
@@ -59,9 +59,21 @@ const _inputDestination = document.getElementById("inputDestination");
 const _dropDownAudioMenuButton = document.getElementById("dropdownMenuButton");
 _inputDestination.value = filePath;
 
+navigator.mediaDevices
+  .enumerateDevices()
+  .then(function (devices) {
+    devices.forEach(function (device) {
+      console.log(
+        device.kind + ": " + device.label + " id = " + device.deviceId
+      );
+    });
+  })
+  .catch(function (err) {
+    console.log(err.name + ": " + err.message);
+  });
+
 async function getAudioSources() {
   return navigator.mediaDevices.enumerateDevices().then((devices) => {
-    console.log(devices);
     audiodevices = devices.filter(
       (d) =>
         d.kind === "audioinput" &&
